6c559e8907d05378ad04653a3129814cd532a234
Mohammad Fadhil Fadholi <73618595+faam05@users.noreply.github.com>
2022-01-06 10:33:45 +0700

    Update Create.vue

--- src/views/post/Create.vue
+++ src/views/post/Create.vue
@@ -73,7 +73,7 @@ export default {
 
             }).catch(error => {
                 //assign state validation with error 
-                validation.value = error.response.data
+                validation.value = error.response.data.data
             })
 
         }
